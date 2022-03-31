18c38518fc5eec9e062327d58a1e260a3dcbc104
Anton Bulankin <antonbulankin7@gmail.com>
2022-03-31 15:46:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Threads",
+    name: "EDNAChatCenter",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Threads",
+            name: "EDNAChatCenter",
             targets: ["Threads"]),
     ],
     dependencies: [
