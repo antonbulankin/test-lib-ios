1e644ea1e5bd35643a9b899bb8223aace3e4547f
Anton Bulankin <antonbulankin7@gmail.com>
2022-03-31 15:33:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EDNAChatCenter",
-            targets: ["EDNAChatCenter"]),
+            targets: ["Threads"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "EDNAChatCenter",
+            name: "Threads",
             url: "https://github.com/ThreadsMobileLib/edna-sdk-ios/releases/download/v4.0.4/threads-4.0.4.zip",
             checksum: "8ca3bbce48f29d96662b63661b05358e2bc7955a5fa868345177920ef02ed970"
         ),
