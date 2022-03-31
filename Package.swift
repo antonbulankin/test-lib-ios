59a96a0d9aef6b769af13afa8cdd93b512ce581c
Anton Bulankin <antonbulankin7@gmail.com>
2022-03-31 15:03:05 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EDNAChatCenter",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "EDNAChatCenter",
+            targets: ["EDNAChatCenter"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "EDNAChatCenter",
+            url: "https://github.com/ThreadsMobileLib/edna-sdk-ios/releases/download/v4.0.4/threads-4.0.4.zip",
+            checksum: "35cec37a3b03821192289361971cc951cfe85bbd04036eed2d4476e4fd2338fd"
+        ),
+    ]
+)
